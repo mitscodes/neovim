42503adb4497a229f173011e553a03923dfc027a
mitscodes <miteshcodes09@gmail.com>
2024-09-23 15:49:01 +0530

    made changes to nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,14 +1,14 @@
 return { -- Nvim-Tree
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("nvim-tree").setup({})
-		-- Nvim-Tree Config
-		vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {})
-		vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>", {})
-	end,
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup({})
+    -- Nvim-Tree Config
+    vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>", { silent = true })
+  end,
 }
